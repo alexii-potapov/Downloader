a59363e2f5d3d31ee506c945e85dabb144050c17
Potapov A.V <a.potapov@ts-soft.ru>
2016-11-17 16:05:29 +0300

    Последовательное скачивание

--- Downloader/Program.cs
+++ Downloader/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using Downloader.Models;
 using Downloader.Utils;
 
@@ -17,9 +19,35 @@ namespace Downloader
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+                return;
             }
+
+            //Todo 
+            //CreateFolder downloadTask.OutputFolder
+
+            var result =StartDownload(downloadTask);
+
+            if (result)
+            {
+                Console.WriteLine("Загрузка завершена успешно");
+            }
+
+
         }
-        
-        
+
+        private static bool StartDownload(DownloadTask downloadTask)
+        {
+            WebClient myWebClient = new WebClient();
+
+            foreach (var link in downloadTask.Links)
+            {
+                var downloadPath = downloadTask.OutputFolder + '\\' + link.Split('/').Last();
+
+                myWebClient.DownloadFile(link, downloadPath);
+            }
+
+            return true;
+
+        }
     }
 }
